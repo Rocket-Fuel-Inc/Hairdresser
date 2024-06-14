feb7431c92f50383bcd164d3ca4e79ca49c12cfd
MarcinMG-web <gierada.marcin@gmail.com>
2024-06-14 17:41:55 +0200

    :feature added png picture

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 import { Box, IconButton, Typography } from '@mui/joy';
 import ColorSchemeToggle from './ColorSchemeToggle';
-import BadgeRoundedIcon from '@mui/icons-material/BadgeRounded';
+import logoIcon from '../assets/logo.png';
 
 export default function Header(): JSX.Element {
   return (
@@ -14,11 +14,11 @@ export default function Header(): JSX.Element {
     >
       <Box sx={{ gap: 2, display: 'flex', alignItems: 'center' }}>
         <IconButton variant='soft' color='primary' size='sm'>
-          <BadgeRoundedIcon />
+          <img src={logoIcon} alt='icon' style={{ width: '20px', height: '20px' }} />
         </IconButton>
-        <Typography level='title-lg'>Company logo</Typography>
+        <Typography level='title-lg'>Hairdresser</Typography>
       </Box>
-      
+
       <ColorSchemeToggle />
     </Box>
   );
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -57,10 +57,9 @@ export default function Dashboard(): JSX.Element {
           backgroundSize: 'cover',
           backgroundPosition: 'center',
           backgroundRepeat: 'no-repeat',
-          backgroundImage: 'url(https://images.unsplash.com/photo-1527181152855-fc03fc7949c8?auto=format&w=1000&dpr=2)',
+          backgroundImage: 'url(src/assets/lightMode.png)',
           [theme.getColorSchemeSelector('dark')]: {
-            backgroundImage:
-              'url(https://images.unsplash.com/photo-1572072393749-3ca9c8ea0831?auto=format&w=1000&dpr=2)',
+            backgroundImage: 'url(src/assets/darkMode.png)',
           },
         })}
       />
